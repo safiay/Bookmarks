cbf4339b0cd5287a2647570157ba07a57d9e83ab
safiay <safiayusuff@gmail.com>
2021-08-05 17:56:37 +0100

    need to get tests passing

--- app.js
+++ app.js
@@ -5,25 +5,54 @@ const { models, sequelize } = require('./models');
 const express = require('express')
 const app = express()
 const port = 3000
+const methodOverride = require("method-override");
 
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
-app.get('/', async (req,res) =>{
+app.get('/', async (req, res) => {
     const bookmark = await models.Bookmark.findAll({})
-    res.render('index',{ bookmark:bookmark })
+    res.render('index', { bookmark: bookmark })
 })
 
-app.post('/', async (req,res) =>{
+app.post('/', async (req, res) => {
     await models.Bookmark.create({
-        name:req.body.name,
-        url:req.body.url
-        
+        name: req.body.name,
+        url: req.body.url
+
     })
     res.redirect('/')
 })
 
-app.listen(port, ()=>{
+app.delete('/:id', async (req, res) => {
+    await models.Bookmark.destroy({
+        where: {
+            id: req.params.id,
+        }
+    })
+    res.redirect('/')
+})
+
+app.put('/:id', async (req, res) => {
+    await models.Bookmark.update({
+        name: req.body.name, url: req.body.url
+    },
+        {
+            where: {
+                id: req.params.id,
+            }
+        })
+    res.redirect('/')
+})
+
+app.get("/update/:id", (req, res) => {
+    res.render("update.ejs", {
+      id: req.params.id,
+    })
+  })
+
+app.listen(port, () => {
     console.log(`listening on ${port}`)
 })
 
--- cypress/integration/bookmark.spec.js
+++ cypress/integration/bookmark.spec.js
@@ -1,12 +1,45 @@
-describe("title screen", function(){
-    it("adds name and url to table when inputing it", function(){
-        cy.task("taskcreateTables")
+
+// fix my tests because they're just not working! sad times :(
+
+
+
+
+
+describe("title screen", function () {
+    beforeEach(function () {
         cy.task("taskTruncateTables")
-        
+    })
+    it("adds name and url to table when inputing it", function () {
+        cy.task("taskcreateTables")
         cy.visit('/')
         cy.get('#name').type('Ramiro')
         cy.get('#url').type('Morales')
         cy.get('#submit').click()
-        cy.get('#table').should("contain", "Ramiro","Safia")
+        cy.get('#table').should("contain", "Ramiro", "Safia")
     })
+    it("delete a url from the table", function () {
+        cy.task("taskcreateTables")
+        cy.visit('/')
+        cy.get("#bookmark-0")
+        cy.get('#bookmark-0-delete').click()
+        cy.get("#bookmark-0").should("not.exist")
+    })
+
+    it("UPDATE: update a specific bookmarl", function () {
+        cy.task("taskCreateBookmark");
+        cy.visit("/");
+        //get first element
+        cy.get("#bookmark-0")
+            .should("contain", "Name: test-name")
+            .should("contain", "URL: test-url.com");
+        //click on the update button and types in new update fields and the check if updated fields were correct
+        cy.get("#bookmark-0-update").click();
+        cy.get("#name").type("test-change-name");
+        cy.get("#url").type("test-change-url.com");
+        cy.get("#submit").click();
+        cy.get("#bookmark-0")
+            .should("contain", "Name: test-change-name")
+            .should("contain", "URL: test-change-url.com");
+    });
 })
+
